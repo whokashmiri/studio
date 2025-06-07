a82b6bae6a5f8a481ba1e7488ea142ba0a386c36
Kha Lifa <meer1khalifa@gmail.com>
2025-06-07 12:03:32 +0000

    for laptops screens and big screens can you make the cards smaller  whic

--- src/components/folder-tree.tsx
+++ src/components/folder-tree.tsx
@@ -78,7 +78,7 @@ export function FolderTreeDisplay({
   }
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {foldersToDisplay.map(folder => (
         <Card 
           key={`folder-${folder.id}`} 
