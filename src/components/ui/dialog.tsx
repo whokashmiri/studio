2dd52ef2c2e44500a5618082eba6a8dbce7331d5
Kha Lifa <meer1khalifa@gmail.com>
2025-06-07 11:08:41 +0000

    the models should take up full width

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-[calc(100vw_-_2rem)] sm:max-w-lg translate-x-[-50%] translate-y-[calc(-50%_-_20px)] sm:translate-y-[calc(-50%_-_35px)] gap-4 border bg-background p-4 sm:p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "fixed left-[50%] top-[50%] z-50 grid w-full sm:w-auto sm:max-w-lg translate-x-[-50%] translate-y-[calc(-50%_-_20px)] sm:translate-y-[calc(-50%_-_35px)] gap-4 border bg-background p-4 sm:p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         "max-h-[90vh] flex flex-col", 
         className
       )}
@@ -122,3 +122,4 @@ export {
   DialogTitle,
   DialogDescription,
 }
+
