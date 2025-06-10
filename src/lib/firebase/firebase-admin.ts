e6c13d7a3b4082a6ae7789dbf4550c2ae9b5d3a7
Kha Lifa <meer1khalifa@gmail.com>
2025-06-10 04:41:28 +0000

    check now, review code may the necessary changes to make it work

--- src/lib/firebase/firebase-admin.ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank as Firebase Admin SDK is being removed.
-// The App Prototyper will handle the deletion of this file if it's no longer referenced,
-// or you can manually delete it.
